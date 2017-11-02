a9033e464f6604ad365cb85ae0b80602cb62d7c5
5397chop <32233354+5397chop@users.noreply.github.com>
2017-11-01 22:41:40 -0500

    week 8

---
+++ geoMessenger/geoMessenger/TablesTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TablesTabBarViewController.swift
+//  geoMessenger
+//
+//  Created by Ivor D. Addo, PhD on 11/1/17.
+//  Copyright © 2017 water. All rights reserved.
+//
+
+import UIKit
+
+class TablesTabBarViewController: UITabBarController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBar.barTintColor = UIColor(red:0.75, green:0.82, blue:0.92, alpha:1.0)
+    }
+        
+
+}
